5ebab33720d6a5f901411a8fa13572412b3c2807
Jones Franco <116607018+Zero9BSC@users.noreply.github.com>
2023-12-11 17:23:36 -0300

    fix(ecommerce-web(Login/Sign up)): :fire: Clear code in firebase.js

--- ecommerce-web/src/config/firebase.js
+++ ecommerce-web/src/config/firebase.js
@@ -1,7 +1,3 @@
-// import { initializeApp } from 'firebase/app';
-// import { getAuth, GoogleAuthProvider } from 'firebase/auth';
-// import firebase from 'firebase/compat/app';
-// import 'firebase/compat/auth';
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
@@ -13,24 +9,6 @@ const firebaseConfig = {
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_APP_ID,
 };
-
-// const app = initializeApp(firebaseConfig);
-// const auth = getAuth(app);
-// const provider = new GoogleAuthProvider();
-// const providers = {
-//     google: provider
-// };
-
-// const handleGoogleLogin = () => {
-//   auth.signInWithRedirect(provider);
-// };
-
-// export { auth, provider, app, providers, handleGoogleLogin };
-
-// Inicializar Firebase
-// const app = firebase.initializeApp(firebaseConfig);
-// export const auth = app.auth();
-// export default app;
 
 const firebaseApp = initializeApp(firebaseConfig);
 const auth = getAuth(firebaseApp);
