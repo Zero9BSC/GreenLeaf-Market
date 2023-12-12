f5e195aaee35788e2a0c3ade591ecfa653cccf7d
Jones Franco <116607018+Zero9BSC@users.noreply.github.com>
2023-12-12 20:06:10 -0300

    feat: :art: Add API and content restructure

--- ecommerce-web/src/pages/Cart.js
+++ ecommerce-web/src/pages/Cart.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/cart.css';
 
 const Cart = () => {
   const cartItems = JSON.parse(localStorage.getItem('cart')) || [];
 
   const handleRemoveItem = (itemId) => {
-    const updatedCart = cartItems.filter((item) => item.id !== itemId);
+    const updatedCart = cartItems.filter((item) => item.code !== itemId);
     localStorage.setItem('cart', JSON.stringify(updatedCart));
     window.location.reload();
   };
 
   const handleReduceQuantity = (itemId) => {
     const updatedCart = cartItems.map((item) => {
-      if (item.id === itemId) {
+      if (item.code === itemId) {
         if (item.quantity > 1) {
           return { ...item, quantity: item.quantity - 1 };
         }
@@ -25,7 +26,7 @@ const Cart = () => {
 
   const handleIncreaseQuantity = (itemId) => {
     const updatedCart = cartItems.map((item) => {
-      if (item.id === itemId) {
+      if (item.code === itemId) {
         return { ...item, quantity: item.quantity + 1 };
       }
       return item;
@@ -41,24 +42,24 @@ const Cart = () => {
         <div className="cart-items">
           <ul className="cart-list">
             {cartItems.map((item) => (
-              <li key={item.id} className="cart-item">
+              <li key={item.code} className="cart-item">
                 <div className="item-details">
-                  <img src={item.image} alt={item.name} className="item-image" />
+                  <img src={item.image_url} alt={item.product_name} className="item-image" />
                   <div className="item-info">
-                    <h3>{item.name}</h3>
+                    <h3>{item.product_name}</h3>
                     <p>Cantidad: {item.quantity}</p>
-                    <p>Precio: ${item.price.toFixed(2)}</p>
+                    <p>Precio: ${item.fixedPrice.toFixed(2)}</p>
                   </div>
                 </div>
                 <div className="item-actions">
-                  <button className="action-button" onClick={() => handleRemoveItem(item.id)}>
+                  <button className="action-button" onClick={() => handleRemoveItem(item.code)}>
                     Eliminar
                   </button>
                   <div className="quantity-modifier">
-                    <button className="quantity-button" onClick={() => handleReduceQuantity(item.id)}>
+                    <button className="quantity-button" onClick={() => handleReduceQuantity(item.code)}>
                       -
                     </button>
-                    <button className="quantity-button" onClick={() => handleIncreaseQuantity(item.id)}>
+                    <button className="quantity-button" onClick={() => handleIncreaseQuantity(item.code)}>
                       +
                     </button>
                   </div>
@@ -69,13 +70,14 @@ const Cart = () => {
           <div className="cart-total">
             <p>
               Total a Pagar: $
-              {cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0).toFixed(2)}
+              {cartItems.reduce((acc, item) => acc + item.fixedPrice * item.quantity, 0).toFixed(2)}
             </p>
           </div>
         </div>
       ) : (
         <p>El carrito está vacío</p>
       )}
+      <Link to="/">Volver al Menú Inicial</Link>
     </div>
   );
 };
--- ecommerce-web/src/pages/Home.js
+++ ecommerce-web/src/pages/Home.js
@@ -1,25 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-const products = [
-  { id: 1, name: 'Producto 1', price: 19.99, image: 'product1.jpg' },
-  { id: 2, name: 'Producto 2', price: 29.99, image: 'product2.jpg' },
-  { id: 3, name: 'Producto 3', price: 24.99, image: 'product3.jpg' },
-  { id: 4, name: 'Oferta Especial 1', price: 9.99, image: 'special1.jpg' },
-  { id: 5, name: 'Oferta Especial 2', price: 14.99, image: 'special2.jpg' }
-];
-
 const Home = () => {
-  const [cartItems, setCartItems] = useState(
-    JSON.parse(localStorage.getItem('cart')) || []
-  );
+  const [cartItems, setCartItems] = useState(JSON.parse(localStorage.getItem('cart')) || []);
+  const [featuredProducts, setFeaturedProducts] = useState([]);
+  const [otherProducts, setOtherProducts] = useState([]);
+
+  useEffect(() => {
+    // Obtener productos aleatorios de la API de Open Food Facts
+    fetch('https://world.openfoodfacts.org/cgi/search.pl?json=1&page_size=6&action=process&sort_by=unique_scans_n')
+      .then((response) => response.json())
+      .then((data) => {
+        // Precios fijos para cada producto
+        const fixedPrices = [
+          19.70,  // Precio fijo para el primer producto
+          14.89,  // Precio fijo para el segundo producto
+          20.00,
+          9.55,
+          35.14,
+          10.38,
+          // ... Continuar con los precios fijos para los demás productos
+        ];
+
+        // Asignar precios fijos y agregar a productos destacados
+        const featured = data.products.slice(0, 3).map((product, index) => ({
+          ...product,
+          fixedPrice: fixedPrices[index],
+        }));
+
+        // Asignar precios fijos y agregar a otras ofertas
+        const others = data.products.slice(3, 6).map((product, index) => ({
+          ...product,
+          fixedPrice: fixedPrices[index + 3], // Sumar 3 para mantener la unicidad de precios
+        }));
+
+        setFeaturedProducts(featured);
+        setOtherProducts(others);
+      })
+      .catch((error) => {
+        console.error('Error fetching products:', error);
+      });
+  }, []);
 
   const handleAddToCart = (item) => {
-    const existingItem = cartItems.find((cartItem) => cartItem.id === item.id);
+    const existingItem = cartItems.find((cartItem) => cartItem.code === item.code);
     if (existingItem) {
       const updatedCart = cartItems.map((cartItem) =>
-        cartItem.id === item.id ? { ...cartItem, quantity: cartItem.quantity + 1 } : cartItem
+        cartItem.code === item.code ? { ...cartItem, quantity: cartItem.quantity + 1 } : cartItem
       );
       setCartItems(updatedCart);
       localStorage.setItem('cart', JSON.stringify(updatedCart));
@@ -33,19 +61,51 @@ const Home = () => {
   return (
     <div className="home">
       <Header cartItems={cartItems} />
+
       <section className="featured-products">
         <h2>Productos Destacados</h2>
         <div className="product-list">
-          {products.map((product) => (
-            <div key={product.id} className="product">
-              <img src={product.image} alt={product.name} />
-              <h3>{product.name}</h3>
-              <p>${product.price.toFixed(2)}</p>
-              <button onClick={() => handleAddToCart(product)}>Añadir al carrito</button>
+          {featuredProducts.map((product) => (
+            <div key={product.code} className="product">
+              <div className="product-img">
+                <img
+                  src={product.image_url}
+                  alt={product.product_name}
+                  className="product-img"
+                />
+              </div>
+              <h3 title={product.product_name}>{product.product_name.substring(0, 25)}</h3>
+              <p>${product.fixedPrice}</p>
+              <button onClick={() => handleAddToCart(product)}>
+                Añadir al carrito
+              </button>
             </div>
           ))}
         </div>
       </section>
+
+      <section className="other-products">
+        <h2>Otras Ofertas</h2>
+        <div className="product-list">
+          {otherProducts.map((product) => (
+            <div key={product.code} className="product">
+              <div className="product-img">
+                <img
+                  src={product.image_url}
+                  alt={product.product_name}
+                  className="product-img"
+                />
+              </div>
+              <h3 title={product.product_name}>{product.product_name.substring(0, 25)}</h3>
+              <p>${product.fixedPrice}</p>
+              <button onClick={() => handleAddToCart(product)}>
+                Añadir al carrito
+              </button>
+            </div>
+          ))}
+        </div>
+      </section>
+
       <Footer />
     </div>
   );
